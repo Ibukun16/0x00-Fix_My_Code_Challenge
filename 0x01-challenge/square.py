299807212b370b9ed77f1286f6bbc0a8365058cf
Ibukun16 <ib.agunbiade18@gmail.com>
2024-07-21 05:10:39 -0700

    Fix my code

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -23,15 +23,27 @@ class Square():
         self.height = height
 
     def area_of_my_square(self):
-        """Calculate and return area of the square"""
-        return self.width * self.width
+        """
+        Calculate area of the square
 
-    def PermiterOfMySquare(self):
-        """Calculate and return perimeter of the square"""
+        Returns:
+        The area of the square
+        """
+        return self.width * self.height
+
+    def perimeter_of_my_square(self):
+        """
+        Calculate perimeter of the square
+
+        Returns:
+        The perimeter of the square.
+        """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """Print the string representation of the square instance"""
+        """
+        Print the string representation of the square instance
+        """
         return "{}/{}".format(self.width, self.height)
 
 
@@ -39,4 +51,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
