80961d545c64a224bffa57cba1b2e757621dbcae
Ibukun16 <ib.agunbiade18@gmail.com>
2024-07-21 03:36:18 -0700

    Fix my code

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
-""" Define class for a square """
+""" A function to find the area and perimeter of a square """
+
 
 class square():
-    """ Description of a square class """
-    
+    """Define a square class"""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
-        """ Define an instance for square """
+        """Initialize a square class"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """Area of the square"""
         return self.width * self.height
 
     def PermiterOfMySquare(self):
-        """ Description of a perimeter instance """
+        """Perimeter instance of the square"""
         return (self.height * 2) + (self.height * 2)
 
     def __str__(self):
+        """Print the objects in string format"""
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    """Create an instance for a square"""
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
